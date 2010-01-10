7e036a6041448f55fb48ee97e0639d6ff0900f19
jde <jde@opal.(none)>
2010-01-10 15:20:16 -0500

    Addition of core functionality.

--- tman.erl
+++ tman.erl
@@ -5,6 +5,8 @@
 -define(NODES_DEFAULT, 900).
 -define(NEIGHBORS_DEFAULT, 20).
 
+-record(node, {id, x, y, random_nodes=[], structured_nodes=[]}).
+
 init() ->
     init(?NODES_DEFAULT, ?NEIGHBORS_DEFAULT).
 
@@ -12,4 +14,10 @@ init(Nodes, Neighbors) ->
     main(Nodes, Neighbors).
 
 main(Nodes, Neighbors) ->
-    io:format("Running T-Man with NODES ~w NEIGHBORS ~w~n", [Nodes, Neighbors]).
+    io:format("Running T-Man with NODES ~w NEIGHBORS ~w~n", [Nodes, Neighbors]),
+    Node1 = #node{id=1,x=2,y=3},
+    Node2 = #node{id=1,x=4,y=1},
+    io:format("Node: ~w ~w ~w~n", [Node1, Node2, node_distance(Node1, Node2)]).
+
+node_distance(N1, N2) ->
+    abs(N1#node.x - N2#node.x) + abs(N1#node.y - N2#node.y).
