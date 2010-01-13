cb926b7bb08c7579047e42efc9006218de80b744
jde <jde@opal.(none)>
2010-01-12 21:39:26 -0500

    Partial completion of functionality for initializing nodes

--- tman.erl
+++ tman.erl
@@ -3,21 +3,52 @@
 -compile(export_all).
 
 -define(NODES_DEFAULT, 900).
--define(NEIGHBORS_DEFAULT, 20).
+-define(DEGREE_DEFAULT, 20).
+-define(CYCLES_DEFAULT, 50).
+-define(SIZE, 30).
 
 -record(node, {id, x, y, random_nodes=[], structured_nodes=[]}).
 
 init() ->
-    init(?NODES_DEFAULT, ?NEIGHBORS_DEFAULT).
+    init(?NODES_DEFAULT, ?DEGREE_DEFAULT, ?CYCLES_DEFAULT, ?SIZE).
 
-init(Nodes, Neighbors) ->
-    main(Nodes, Neighbors).
+init(Nodes, Degree, Cycles, Size) ->
+    main(Nodes, Degree, Cycles, Size).
 
-main(Nodes, Neighbors) ->
-    io:format("Running T-Man with NODES ~w NEIGHBORS ~w~n", [Nodes, Neighbors]),
-    Node1 = #node{id=1,x=2,y=3},
-    Node2 = #node{id=1,x=4,y=1},
-    io:format("Node: ~w ~w ~w~n", [Node1, Node2, node_distance(Node1, Node2)]).
+main(Nodes, Degree, Cycles, Size) ->
+    io:format("Running T-Man with NODES ~w of DEGREE  ~w over CYCLES ~w in space of SIZE ~w ~n", 
+	      [Nodes, Degree, Cycles, Size]).
+
+init_nodes(NodeNumber, Degree, Size) ->
+    init_nodes(NodeNumber, Degree, Size, []).
+
+init_nodes(0, Degree, _, Nodes) ->
+    init_nodes_neighbors(Nodes, Degree);
+init_nodes(NodeNumber, Degree, Size, Nodes) ->
+    init_nodes(NodeNumber - 1, Degree, Size, [#node{id=NodeNumber,x=1,y=2}|Nodes]).  
+
+init_nodes_neighbors(Nodes, Degree) ->
+    [Nodes, Degree].
+
+generate_random_unique_coordinate(NodeNumber, Size, _) when (NodeNumber > Size * Size) ->
+    exit(too_many_nodes);
+generate_random_unique_coordinate(NodeNumber, _, Coordinates) when (length(Coordinates) >= NodeNumber) ->
+    exit(available_coordinates_exhausted);
+generate_random_unique_coordinate(NodeNumber, Size, Coordinates) ->
+    generate_random_unique_coordinate(NodeNumber, Size, Coordinates, []).
+
+generate_random_unique_coordinate(NodeNumber, Size, Coordinates, []) ->
+    Coord = [generate_random_coordinate(Size), generate_random_coordinate(Size)],
+    case coordinate_is_unique(Coord, Coordinates) of
+	true -> [Coord, [Coord|Coordinates]];
+	false -> generate_random_unique_coordinate(NodeNumber, Size, Coordinates, [])
+    end.
+
+generate_random_coordinate(Size) ->
+    random:uniform(Size) - 1.
+
+coordinate_is_unique(Coord, Coordinates) ->
+    not lists:member(Coord, Coordinates).
 
 node_distance(N1, N2) ->
     abs(N1#node.x - N2#node.x) + abs(N1#node.y - N2#node.y).
